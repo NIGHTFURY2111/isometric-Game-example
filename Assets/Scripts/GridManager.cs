9aa05e1c1b9ac68d658deb95d1ac1ccbd453fe26
NIGHTFURY2111 <65185393+NIGHTFURY2111@users.noreply.github.com>
2024-07-04 19:45:06 +0530

    Initial commit

---
+++ Assets/Scripts/GridManager.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.TextCore.LowLevel;
+
+public class GridManager : MonoBehaviour
+{
+    public GameObject tilePrefab;
+    public int gridSize;
+    void Start () 
+    {
+        GenerateGrid();
+    }
+    void GenerateGrid()
+    {//generates a grid of size "gridSize" from origin 
+        for (int x = 0; x < gridSize; x++)
+        {
+            for (int y = 0; y < gridSize; y++)
+            {
+                GameObject tile = Instantiate(tilePrefab, new Vector3(x, 0, y), Quaternion.identity, transform);
+                TileInfo tileInfo = tile.GetComponent<TileInfo>();
+                tileInfo.SetPosition(x, y);
+                tile.name = "Tile_" + x + "_" + y;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/TileInfo.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileInfo : MonoBehaviour
+{   
+    //  x and y coordinates of the tile
+    int _x, _y; 
+
+    public void SetPosition(int x, int y)
+    {
+        this._x = x;
+        this._y = y;
+    }
+
+    #region Getters
+    public int x { get =>_x; }
+    public int y { get =>_y; }
+    #endregion
+}
